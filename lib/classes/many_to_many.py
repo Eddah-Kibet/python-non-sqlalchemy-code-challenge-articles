312968d7e2a99de94b98fce349c853c20c6241b5
Eddah kibet <eddahkibet20@gmail.com>
2025-08-20 22:12:15 +0300

    first commit

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,12 +1,56 @@
 class Article:
+    all = []
     def __init__(self, author, magazine, title):
+        if not isinstance(title,str):
+            return "title must be a string"
+        if (2<= len(title) <= 16):
+            return "title must have characters between 2 and 16"
+
         self.author = author
         self.magazine = magazine
-        self.title = title
+        self._title = title
+        Article.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self,value):
+        if hasattr(self,"_title"):
+            return "title cannot be changed after instanciation"
+        if not isinstance(value,str):
+            return "title must be a string"
+        if (2 <= len(value) <= 16):
+            return "title must be a string of characters between 2 an 16"
+        
+        self._title = value
+        
         
 class Author:
     def __init__(self, name):
-        self.name = name
+        if not isinstance(name, str):
+            return "name must be of type str"
+        if len(name) == 0:
+            return "name must be longer than 0 characters"
+        
+        self._name = name
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def title(self,value):
+        if hasattr(self,"_name"):
+            return "name cannot be changed after instanciation"
+        if not isinstance(value,str):
+            return "name must be a string"
+        if (2 <= len(value) <= 16):
+            return "name must be a string of more than one character"
+        
+        self._name = value
+    
 
     def articles(self):
         pass
